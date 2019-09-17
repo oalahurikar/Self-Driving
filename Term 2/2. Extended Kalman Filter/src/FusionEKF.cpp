4e4398f34c525f542bf7ae377c2bab1ee8f19018
Omkar <oalahuri@mtu.edu>
2019-09-17 16:47:41 -0400

    Term 2 init

---
+++ Term 2/2. Extended Kalman Filter/src/FusionEKF.cpp
@@ -0,0 +1,160 @@
+#include "FusionEKF.h"
+#include "tools.h"
+#include "Eigen/Dense"
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+using std::vector;
+
+/*
+ * Constructor.
+ */
+FusionEKF::FusionEKF() {
+  is_initialized_ = false;
+
+  previous_timestamp_ = 0;
+
+  // initializing matrices
+  R_laser_ = MatrixXd(2, 2);
+  R_radar_ = MatrixXd(3, 3);
+  H_laser_ = MatrixXd(2, 4);
+  Hj_ = MatrixXd(3, 4);
+
+  // measurement covariance matrix - laser
+  R_laser_ << 0.015,      0,
+                   0, 0.015;
+
+  // measurement covariance matrix - radar
+  R_radar_ << 0.07,      0,    0,
+                 0, 0.0009,    0,
+                 0,      0, 0.07;
+
+  // measurement matrix - laser
+  H_laser_ << 1, 0, 0, 0,
+	      0, 1, 0, 0;
+
+  //
+  // Finish initializing the FusionEKF.
+  // Set the process and measurement noises
+  //
+
+  // create a 4D state vector
+  ekf_.x_ = VectorXd(4);
+
+  // state covariance matrix P
+  ekf_.P_ = MatrixXd(4, 4);
+  ekf_.P_ << 1, 0,    0,    0,
+             0, 1,    0,    0,
+             0, 0, 1000,    0,
+             0, 0,    0, 1000;
+  
+  // create the process noise covariance matrix Q
+  ekf_.Q_ = MatrixXd(4, 4);
+
+  // the initial transition matrix F_
+  ekf_.F_ = MatrixXd(4, 4);
+  ekf_.F_ << 1, 0, 1, 0,
+             0, 1, 0, 1,
+             0, 0, 1, 0,
+             0, 0, 0, 1;
+}
+
+/**
+* Destructor.
+*/
+FusionEKF::~FusionEKF() {}
+
+void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
+
+  /*****************************************************************************
+   *  Initialization
+   ****************************************************************************/
+  if (!is_initialized_) {
+    //
+    // Initialize the state ekf_.x_ with the first measurement.
+    // Create the covariance matrix.
+    // Remember: you'll need to convert radar from polar to cartesian coordinates.
+    //
+
+    previous_timestamp_ = measurement_pack.timestamp_;
+
+    // first measurement
+    cout << "EKF: " << endl;
+    ekf_.x_ = VectorXd(4);
+    ekf_.x_ << 1, 1, 1, 1;
+
+    if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
+      // Convert radar from polar to cartesian coordinates and initialize state.
+      float ro = measurement_pack.raw_measurements_[0];
+      float phi = measurement_pack.raw_measurements_[1];
+      float ro_prime = measurement_pack.raw_measurements_[2];
+
+      ekf_.x_[0] = ro * cos(phi);
+      ekf_.x_[1] = ro * sin(phi);
+      ekf_.x_[2] = ro_prime * cos(phi);
+      ekf_.x_[3] = ro_prime * sin(phi);
+    }
+    else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
+      ekf_.x_[0] = measurement_pack.raw_measurements_[0];
+      ekf_.x_[1] = measurement_pack.raw_measurements_[1];
+    }
+
+    // done initializing, no need to predict or update
+    is_initialized_ = true;
+    return;
+  }
+
+  // dt needs to be in seconds, but timestamp is in microseconds
+  float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;
+  previous_timestamp_ = measurement_pack.timestamp_;
+
+  /*****************************************************************************
+   *  Prediction
+   ****************************************************************************/
+
+  // 1. Modify the F matrix so that the time is integrated
+  ekf_.F_(0, 2) = dt;
+  ekf_.F_(1, 3) = dt;
+
+  // 2. Set the process noise covariance matrix Q
+  float noise_ax = 9;
+  float noise_ay = 9;
+
+  float dt2 = dt * dt;
+  float dt3 = dt2 * dt;
+  float dt4 = dt2 * dt2;
+
+  float dt4_4 = dt4 / 4;
+  float dt3_2 = dt3 / 2;
+
+  ekf_.Q_ << dt4_4 * noise_ax,                0, dt3_2 * noise_ax,                0,
+                            0, dt4_4 * noise_ay,                0, dt3_2 * noise_ay,
+             dt3_2 * noise_ax,                0,   dt2 * noise_ax,                0,
+                            0, dt3_2 * noise_ay,                0,   dt2 * noise_ay;
+
+  // 3. Call the Kalman Filter predict() function
+  ekf_.Predict();
+
+  /*****************************************************************************
+   *  Update
+   ****************************************************************************/
+
+  if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
+    // Radar updates
+    ekf_.R_ = R_radar_;
+    ekf_.H_ = tools.CalculateJacobian(ekf_.x_);
+    ekf_.UpdateEKF(measurement_pack.raw_measurements_);
+  } else {
+    // Laser updates
+    ekf_.R_ = R_laser_;
+    ekf_.H_ = H_laser_;
+    ekf_.Update(measurement_pack.raw_measurements_);
+  }
+
+  // print the output
+  cout << "x_ = " << ekf_.x_ << endl;
+  cout << "P_ = " << ekf_.P_ << endl;
+}
---
+++ Term 2/3. Unscented Kalman Filter/cmake-build-debug/CMakeFiles/UnscentedKF.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/olahurikar/OneDrive/Self Driving/Udacity/Term 2/Assignments/Assignment 2/CarND-Unscented-Kalman-Filter-Project-master/src/main.cpp" "C:/Users/olahurikar/OneDrive/Self Driving/Udacity/Term 2/Assignments/Assignment 2/CarND-Unscented-Kalman-Filter-Project-master/cmake-build-debug/CMakeFiles/UnscentedKF.dir/src/main.cpp.obj"
+  "C:/Users/olahurikar/OneDrive/Self Driving/Udacity/Term 2/Assignments/Assignment 2/CarND-Unscented-Kalman-Filter-Project-master/src/tools.cpp" "C:/Users/olahurikar/OneDrive/Self Driving/Udacity/Term 2/Assignments/Assignment 2/CarND-Unscented-Kalman-Filter-Project-master/cmake-build-debug/CMakeFiles/UnscentedKF.dir/src/tools.cpp.obj"
+  "C:/Users/olahurikar/OneDrive/Self Driving/Udacity/Term 2/Assignments/Assignment 2/CarND-Unscented-Kalman-Filter-Project-master/src/ukf.cpp" "C:/Users/olahurikar/OneDrive/Self Driving/Udacity/Term 2/Assignments/Assignment 2/CarND-Unscented-Kalman-Filter-Project-master/cmake-build-debug/CMakeFiles/UnscentedKF.dir/src/ukf.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
